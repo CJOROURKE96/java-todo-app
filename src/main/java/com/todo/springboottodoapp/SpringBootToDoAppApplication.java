3f3d06410f0aa24fcd1fd9bb6ea21037cdd88829
Connor  O'Rourke <connororourke@Connors-MacBook-Pro.local>
2024-02-15 17:36:46 +0000

    add all directories with files from spring.io

---
+++ src/main/java/com/todo/springboottodoapp/SpringBootToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.todo.springboottodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/springboottodoapp/SpringBootToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.springboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
